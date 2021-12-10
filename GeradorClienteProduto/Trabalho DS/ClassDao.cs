67959622c925ca8d5bca6f846f7ca4e484d717e8
Victor Angelo Legat Cerqueira <91611155+Brenaki@users.noreply.github.com>
2021-12-10 14:32:31 -0300

    GeradorClienteProduto

---
+++ GeradorClienteProduto/Trabalho DS/ClassDao.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Trabalho_DS
+{
+    public class Campos
+    {
+        public int Id;
+        public string Nome;
+        public decimal V_Compras;
+        public string P_Comprado;
+    }
+
+    public class ClassDao
+    {
+        public ClassDao()
+        {
+        }
+
+        public Campos campos = new Campos();
+
+        public MySqlConnection minhaConexao;
+
+        public string usuarioBD = "root";
+        public string senhaBd = "admin";
+        public string servidor = "localhost";
+        string bancoDados;
+        string tabela;
+
+        public void conecte(string BancoDados, string Tabela)
+        {
+            bancoDados = BancoDados;
+            tabela = Tabela;
+            minhaConexao = new MySqlConnection("server=" + servidor + "; database=" + bancoDados +
+                                                "; uid=" + usuarioBD + "; password=" + senhaBd);
+        }
+
+        void Abrir()
+        {
+            minhaConexao.Open();
+        }
+        void Fechar()
+        {
+            minhaConexao.Close();
+        }
+
+        public void PreencherTabela(System.Windows.Forms.DataGridView dgv)
+        {
+            Abrir();
+
+            MySqlDataAdapter meuAdapter = new MySqlDataAdapter("Select * From " + tabela, minhaConexao);
+            System.Data.DataSet dataSet = new System.Data.DataSet();
+            dataSet.Clear();
+            meuAdapter.Fill(dataSet, tabela);
+            dgv.DataSource = dataSet;
+            dgv.DataMember = tabela;
+
+            Fechar();
+        }
+
+        public void Inserir(string campoNome, decimal V_Compras, string P_Comprado)
+        {
+            Abrir();
+
+            MySqlCommand comando = new MySqlCommand("INSERT INTO " + tabela + "(`Nome`, `V_Compras`, `P_Comprado`) " +
+                "VALUES (@nome, @VCompras, @PComprado)", minhaConexao);
+
+            comando.Parameters.AddWithValue("@nome", campoNome);
+            comando.Parameters.AddWithValue("@VCompras", V_Compras);
+            comando.Parameters.AddWithValue("@PComprado", P_Comprado);
+
+            comando.ExecuteNonQuery();
+
+            Fechar();
+        }
+    }
+}
---
+++ GeradorClienteProduto/Trabalho DS/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Trabalho_DS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ClassDao dao = new ClassDao();
+
+        public static MySqlConnection minhaconexao;
+
+        public String[] ProdutosList = { "Coca-Cola", "Bolacha Takinas", "Toca de Lã", "Barra de Chocolate Garota",
+            "Gif Card Free Fire", "Alface Crespa", "Massa de Tomate", "Batata Doce", "Peito de Frago", "Carne de Jabuti"};
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                LoadFiles();
+                conectarBD();
+            }
+            catch
+            {
+                conectarBD();
+                NomeList.SaveFile("NomeList.txt", RichTextBoxStreamType.PlainText);
+                Sb1List.SaveFile("Sb1List.txt", RichTextBoxStreamType.PlainText);
+                Sb2List.SaveFile("Sb2List.txt", RichTextBoxStreamType.PlainText);
+                richTextBox1.SaveFile("MD.txt", RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void conectarBD()
+        {
+            try
+            {
+                dao.conecte("bdmassa", "M_Cliente");
+                dao.PreencherTabela(dataGridView1);
+            }
+            catch
+            {
+                MessageBox.Show("Erro ao conectar com BD!!");
+                Close();
+            }
+        }
+
+        private void SaveFile(RichTextBox list)
+        {
+            list.SaveFile(list.Name + ".txt", RichTextBoxStreamType.PlainText);
+        }
+
+        private void LoadFiles()
+        {
+            NomeList.LoadFile("NomeList.txt", RichTextBoxStreamType.PlainText);
+            Sb1List.LoadFile("Sb1List.txt", RichTextBoxStreamType.PlainText);
+            Sb2List.LoadFile("Sb2List.txt", RichTextBoxStreamType.PlainText);
+            richTextBox1.LoadFile("MD.txt", RichTextBoxStreamType.PlainText);
+        }
+
+        private void BtNome_Click(object sender, EventArgs e)
+        {
+            if (TxtNome.Text != "")
+            {
+                if (NomeList.Text == "")
+                    NomeList.Text += TxtNome.Text;
+                else
+                    NomeList.Text += "\n" + TxtNome.Text;
+                SaveFile(NomeList);
+                TxtNome.Clear();
+            }
+        }
+
+        private void BtSb1_Click(object sender, EventArgs e)
+        {
+            if (TxtSb1.Text != "")
+            {
+                if (Sb1List.Text == "")
+                    Sb1List.Text += TxtSb1.Text;
+                else
+                    Sb1List.Text += "\n" + TxtSb1.Text;
+                SaveFile(Sb1List);
+                TxtSb1.Clear();
+            }
+        }
+
+        private void BtSb2_Click(object sender, EventArgs e)
+        {
+            if (TxtSb2.Text == "")
+            {
+                TxtSb2.Text = "*";
+            }
+
+            if (Sb2List.Text == "")
+                Sb2List.Text += TxtSb2.Text;
+            else
+                Sb2List.Text += "\n" + TxtSb2.Text;
+
+            SaveFile(Sb2List);
+
+            TxtSb2.Clear();
+        }
+
+        private void BtGM_Click(object sender, EventArgs e)
+        {
+            int NumMassa = 0;
+
+            if (TxtGmNum.Text == "")
+            {
+                NumMassa = 1;
+            }
+            else
+            {
+                NumMassa = int.Parse(TxtGmNum.Text);
+            }
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < NumMassa; i++)
+            {
+                //gerar nome
+                int RandomName = rnd.Next(0, NomeList.Lines.Count() - 1);
+                int RandomSb1 = rnd.Next(0, Sb1List.Lines.Count() - 1);
+                int RandomSb2 = rnd.Next(0, Sb2List.Lines.Count() - 1);
+
+                string nome = NomeList.Lines[RandomName];
+                string Sb1 = Sb1List.Lines[RandomSb1];
+                string Sb2 = Sb2List.Lines[RandomSb2];
+
+                String NomeC;
+
+                if (Sb2 == "*")
+                    NomeC = nome + " " + Sb1;
+                else
+                    NomeC = nome + " " + Sb1 + " " + Sb2;
+
+                if (richTextBox1.Text == "")
+                    richTextBox1.Text += NomeC + "\t";
+                else
+                    richTextBox1.Text += "\n" + NomeC + "\t";
+
+                //Gerar Valor de compras
+                decimal VCompras = rnd.Next(0, 150000);
+                richTextBox1.Text += VCompras + "\t";
+
+                //Gerar produto mais comprado aleatorio
+                string PComprado = ProdutosList[rnd.Next(0, ProdutosList.Count())];
+                richTextBox1.Text += PComprado;
+
+                //passar por bd
+                dao.Inserir(NomeC, VCompras, PComprado);
+            }
+
+            dao.PreencherTabela(dataGridView1);
+            richTextBox1.SaveFile("MD.txt", RichTextBoxStreamType.PlainText);
+            MessageBox.Show("Dados foram alocados no banco de dados", "Suscesso!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void BtMCliente_Click(object sender, EventArgs e)
+        {
+            decimal MaiorVolume = 0;
+            int MelhorClienteLine = 0;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                decimal CurrentVolume = decimal.Parse(dataGridView1.Rows[i].Cells[2].Value.ToString());
+
+                if (CurrentVolume > MaiorVolume)
+                {
+                    MaiorVolume = CurrentVolume;
+                    MelhorClienteLine = i;
+                }
+            }
+
+            ListBoxMCliente.Text = dataGridView1.Rows[MelhorClienteLine].Cells[1].Value.ToString() + " | " + 
+                dataGridView1.Rows[MelhorClienteLine].Cells[2].Value.ToString() + " | " + 
+                dataGridView1.Rows[MelhorClienteLine].Cells[3].Value.ToString();
+        }
+    }
+}
